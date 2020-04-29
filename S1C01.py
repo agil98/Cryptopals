1d900c4487befbe8daa6ae5a573392735a996ed5
Alexandra Gil <34052412+agil98@users.noreply.github.com>
2020-04-28 21:15:40 -0700

    Update S1C01.py

--- S1C01.py
+++ S1C01.py
@@ -1,7 +1,7 @@
 import codecs
 
 def hex_to_base64(s):
-  return odecs.encode(codecs.decode(s, 'hex'), 'base64').decode()
+  return codecs.encode(codecs.decode(s, 'hex'), 'base64').decode()
 
 def main():
     print(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
